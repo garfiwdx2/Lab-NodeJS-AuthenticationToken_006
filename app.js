9f7a6ce9046b43391a6d35e3b1c4c0eacad5ce35
garfiwdx2 <garfiwd15@gmail.com>
2026-01-20 13:19:51 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-
-const bookRoutes = require('./routes/bookRoutes'); // กําหนดการเรียกโมดูล bookRoutes
+import express from 'express';
+import cors from 'cors';
+// const express = require('express');
+// const cors = require('cors');
+// const bookRoutes = require('./routes/bookRoutes'); // กําหนดการเรียกโมดูล bookRoutes
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+import bookRoutes from './routes/bookRoutes.js';
 app.use('/api/books', bookRoutes); // กําหนดการเรียกใช้งาน API ของ books
 
+import authRoutes from './routes/authRoutes.js';
+app.use('/api/auth', authRoutes);
+
+// app.use('/api/books', bookRoutes); // กําหนดการเรียกใช้งาน API ของ books
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
  console.log(`Server is running on port ${PORT}`);
